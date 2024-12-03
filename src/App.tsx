51262579b27e9886835da6ab7eb26bc6e1f0f9d1
faizal <faisalt661@gmail.com>
2024-12-03 20:26:40 +0530

    created forms

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import { useState } from "react";
+import "./App.css";
+import { ImageHeader } from "./components/imageHeader";
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="h-[100vh] flex flex-col justify-center items-center">
+      <ImageHeader />
+      <div className="w-[350px]">
+        <form className="mb-5">
+          <input
+            type="text"
+            name=""
+            id=""
+            className="bg-gray-700 w-full mb-3 rounded-md p-2"
+          />
+          <button type="submit" className="bg-gray-500 w-1/2 p-2 rounded-sm">
+            Add
+          </button>
+        </form>
+        <div></div>
       </div>
-      <h1>Vite + React js</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
